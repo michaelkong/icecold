390c66e6c2661cf254f5d64b7ff1ed0ea6aee6db
Michael Kong <michael.kong@23992-mbpr.local>
2015-10-08 13:36:36 -0500

    Added Basic Authentication

---
+++ src/main/java/com/slalom/icecold/domain/UserAccount.java
@@ -0,0 +1,53 @@
+package com.slalom.icecold.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.Id;
+
+@JsonIgnoreProperties({"userName","password" })
+public class UserAccount {
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(final String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+}
