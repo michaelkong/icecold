0a9d7b1ce2f162e1bcfcf9b19b0004a3e60881d0
Michael Kong <michael.kong@23864-e7440.2d.hq>
2015-11-02 16:44:47 -0600

    Enabled logback, added notes to readme

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
 # icecold
 
 To run the app with maven
+1.
 mvn clean install
+2. Once the Maven build is successful, run following to start the server
 mvn spring-boot:run
 
+
+# Setup Mongo DB
+1. Install Mongo using brew
+brew install mongodb
+
+2. Create the mongo mongo folder
+run this in command line:
+$ sudo mkdir -p /data/db/
+
+3. Start Mongo DB
+run this in command line:
+mongod
+
+4. Once the Mongo DB is running, open a new command line window to run the mongo import to set up the icecold database. Please remember to change the folder path to match your local environment
+
+mongoimport --host=127.00.1 --db icecold --collection flavor < /Users/michael.kong/icecold/src/main/resources/flavor.json
+
+mongoimport --host=127.00.1 --db icecold --collection userAccount < /Users/michael.kong/icecold/src/main/resources/userAccount.json
+
+mongoimport --host=127.00.1 --db icecold --collection inventory < /Users/michael.kong/icecold/src/main/resources/inventory.json
+
+5. Accessing the Mongo DB
 
--- src/main/java/com/slalom/icecold/IceColdApplication.java
+++ src/main/java/com/slalom/icecold/IceColdApplication.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -25,43 +26,51 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @Configuration
 @ComponentScan("com.slalom.icecold")
 @EnableAutoConfiguration( )
-//Comment out the above line and uncomment below when disable security
+//To disable security, comment out the above line and uncomment below.
 //@EnableAutoConfiguration(exclude = { org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class })
 public class IceColdApplication {
 
-//    @Value("${db.mongo.database}")
-//    private String mongoDatabase;
-//    @Value("${db.mongo.host}")
-//    private String mongoHost;
-//    @Value("${db.mongo.username:''}")
-//    private String username;
-//    @Value("${db.mongo.password:''}")
-//    private String password;
+    @Value("${db.mongo.database}")
+    private String mongoDatabase;
+    @Value("${db.mongo.host}")
+    private String mongoHost;
+    @Value("${db.mongo.username:''}")
+    private String username;
+    @Value("${db.mongo.password:''}")
+    private String password;
 
     public static void main(String[] args) {
 
-        SpringApplication.run(IceColdApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(IceColdApplication.class, args);
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
 
-//    @Bean
-//    public MongoDbFactory mongoDbFactory() throws Exception {
-//
-//        return new SimpleMongoDbFactory(new MongoClient(mongoHost), mongoDatabase);
-//    }
-//
-//    @Bean
-//    public MongoTemplate mongoTemplate() throws Exception {
-//        return new MongoTemplate(mongoDbFactory());
-//    }
+    @Bean
+    public MongoDbFactory mongoDbFactory() throws Exception {
+
+        return new SimpleMongoDbFactory(new MongoClient(mongoHost), mongoDatabase);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongoDbFactory());
+    }
 
 }
 
 //Comment out the following two WebSecurityConfig classes to disable Spring security
-///*
 @Configuration
 class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
 
@@ -104,4 +113,3 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 }
-//*/
