fc2508c7d8df8ae5161387bf1565c138a7f485b4
Michael Kong <michael.kong@23992-mbpr.local>
2015-12-10 11:21:49 -0600

    Create a failed build for testing

--- src/main/java/com/slalom/icecold/IceColdApplication.java
+++ src/main/java/com/slalom/icecold/IceColdApplication.java
@@ -114,13 +114,4 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 }
 
-class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().fullyAuthenticated().and().
-                httpBasic().and().
-                csrf().disable();
-    }
-
-}
+blah blah
